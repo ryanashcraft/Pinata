eede19befe7ce18bc6311716219e7080591df745
Ryan Ashcraft <ryanashcraft@gmail.com>
2012-07-24 10:00:03 +0200

    Remove functionality preventing closing pinned tabs. Allow opening new windows from tabs in same domain.

--- background.js
+++ background.js
@@ -1,11 +1,8 @@
 
-var pinnedTabs = [];
 var mainWindow;
 
 chrome.tabs.onUpdated.addListener(function(tabId, updateInfo, tab) {
 	var url = updateInfo.url;
-
-	setPinnedTabs();
 
 	if (url != undefined) {
 		checkForHit(url, tab);
@@ -15,89 +12,36 @@ chrome.tabs.onUpdated.addListener(function(tabId, updateInfo, tab) {
 chrome.tabs.onCreated.addListener(function(tab) {
 	var url = tab.url;
 
-	setPinnedTabs();
-
 	if (url != undefined) {
 		checkForHit(url, tab);
 	}
 });
 
-chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-	var url, index;
-	var closedTabIndex;
-	for (i in pinnedTabs) {
-		if (pinnedTabs[i].id == tabId) {
-			url = pinnedTabs[i].url;
-			index = pinnedTabs[i].index;
-			closedTabIndex = i;
-			break;
-		}
-	}
+// chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+// 	var url, index;
+// 	var closedTabIndex;
 
-	if (url != undefined) {
-		chrome.tabs.create({
-			url: url,
-			pinned: true,
-			active: false
-			// index: index
-		}, function(tab) {
-			pinnedTabs[closedTabIndex].id = tab.id;
-		})
-	}
-});
+// 	if (url != undefined) {
+// 		chrome.tabs.create({
+// 			url: url,
+// 			pinned: true,
+// 			active: false
+// 			// index: index
+// 		}, function(tab) {
+// 			pinnedTabs[closedTabIndex].id = tab.id;
+// 		})
+// 	}
+// });
 
 function init() {
-	setPinnedTabs();
+	chrome.windows.getCurrent(function(w) {
+		mainWindow = w;
+	});
 }
-
-chrome.tabs.onActivated.addListener(function(activeInfo) {
-	setPinnedTabs();
-});
-
-chrome.tabs.onHighlighted.addListener(function(highlightInfo) {
-	setPinnedTabs();
-});
-
-chrome.tabs.onAttached.addListener(function(tabId, attachInfo) {
-	setPinnedTabs();
-});
-
-chrome.tabs.onDetached.addListener(function(tabId, detachInfo) {
-	setPinnedTabs();
-});
 
 chrome.tabs.onMoved.addListener(function(tabId, moveInfo) {
-	setPinnedTabs({
-		windowId: moveInfo.windowId,
-		tabId: tabId,
-		toIndex: moveInfo.toIndex
-	});
+	
 });
-
-function setPinnedTabs(updateInfo) {
-	chrome.windows.getCurrent({
-		populate: true
-	}, function(w) {
-		mainWindow = w;
-
-		for (j in w.tabs) {
-			var t = w.tabs[j];
-			if (t.pinned) {
-				var toIndex = t.index;
-
-				if (updateInfo != undefined && (w.id == updateInfo.windowId && t.id == updateInfo.tabId)) {
-					toindex = updateInfo.toIndex;
-				}
-
-				pinnedTabs.push({
-					id: t.id,
-					url: t.url,
-					index: toIndex
-				});
-			}
-		}
-	});
-}
 
 function urlMatch(urlA, urlB) {
 	var domainRegex = /^(([^:\/?#]+):)?(\/\/([^\/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?/igm;
@@ -128,7 +72,7 @@ function checkForHit(url, tab) {
 				var t = w.tabs[j];
 				if (tab.id != t.id
 					&& t.pinned
-					&& t.windowId == mainWindow.id) {
+					&& tab.windowId == mainWindow.id) {
 					var match = urlMatch(url, t.url);
 					if (match == "full") {
 						hit(tab.id, t, t.windowId);
